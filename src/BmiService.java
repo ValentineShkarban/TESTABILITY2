23c44f87fa2ba2d40af53b5ea75bf007a07b2e9e
Valentine <vals91@outlook.com>
2024-08-26 18:49:58 +0300

    commit

---
+++ src/BmiService.java
@@ -0,0 +1,8 @@
+public class BmiService {
+
+    public int calculate(int weightInKg, double heightInMeters) {
+        double index = weightInKg / heightInMeters / heightInMeters;
+        return (int) index;
+
+    }
+}
